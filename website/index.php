3d44682de535d170fb4392922dbeb94781c0a908
Claudinei <cborgesleal@gmail.com>
2019-11-06 16:40:20 -0300

    Fragmentando o Template Index.html

--- website/index.php
+++ website/index.php
@@ -1,1 +1,19 @@
-<?php phpinfo() ?>
+<?php 
+
+require_once 'layout/script.php';
+require_once 'layout/conteudo.php';
+
+echo $head;
+echo $nav;
+echo $aside;
+
+echo '<div class="content-wrapper">';
+
+echo '</div>';
+
+//echo $conteudo;
+echo $footer;
+echo $aside2;
+echo $javascript;
+
+?>
